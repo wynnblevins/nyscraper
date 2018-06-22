c0db2b9175f398d2f07ae7ccb174a745ff84349d
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-22 18:02:29 -0400

    Adding yarn dependencies to project

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const db = require('./models');
+const mongoose = require('mongoose');
+const cheerio = require('cheerio');
+const request = require('request');
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,8 +18,12 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 
-// Send every other request to the React app
+
 // Define any API routes before this runs
+require('./controllers/articlesController')(app, db);
+require('./controllers/notesController')(app, db);
+require('./controllers/scraperController')(app, cheerio, request)
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
